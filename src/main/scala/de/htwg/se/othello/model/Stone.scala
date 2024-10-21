4dff6345a34007eaa0fb44b6674fdcec04f67dd3
imranjan75 <janimran7599@gmail.com>
2024-10-21 15:09:35 +0200

    override toString

--- src/main/scala/de/htwg/se/othello/model/Stone.scala
+++ src/main/scala/de/htwg/se/othello/model/Stone.scala
@@ -2,4 +2,9 @@ package de.htwg.se.othello.model
 
 enum Stone {
   case Black, White, Empty
+
+   override def toString: String = this match
+    case Black => "S"    
+    case White => "W"
+    case Empty => "."
 }
