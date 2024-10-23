111b92d21bcbb93bf7d559886f7bec71ebe85c0b
imranjan75 <janimran7599@gmail.com>
2024-10-23 19:25:50 +0200

    optimize construktur of board class

--- src/main/scala/de/htwg/se/othello/model/Board.scala
+++ src/main/scala/de/htwg/se/othello/model/Board.scala
@@ -3,15 +3,18 @@ package de.htwg.se.othello.model
 class Board(private val board: Vector[Vector[Stone]]) {
 
     def this(row: Int, column: Int) = {
-    this(Vector.fill(row, column)(Stone.Empty))
-    val pos1 = (row - 1) / 2
-    val pos2 = ( column - 1) / 2
+    this({
+        val emptyBoard = Vector.fill(row, column)(Stone.Empty)
+        val pos1 = (row - 1) / 2
+        val pos2 = ( column - 1) / 2
 
-    val new_board = board
-      .updated(pos1, board(pos1).updated(pos2, Stone.White)) // Platziere den ersten weißen Stein
-      .updated(pos1, board(pos1).updated(pos2 + 1, Stone.Black)) // Platziere den ersten schwarzen Stein
-      .updated(pos1 + 1, board(pos1 + 1).updated(pos2, Stone.Black)) // Platziere den zweiten schwarzen Stein
-      .updated(pos1 + 1, board(pos1 + 1).updated(pos2 + 1, Stone.White)) // Platziere den zweiten weißen Stein
+        val updatedBoard = emptyBoard
+        .updated(pos1, emptyBoard(pos1).updated(pos2, Stone.White).updated(pos2 + 1,Stone.Black)) // Platziere den ersten weißen Stein
+        .updated(pos1 + 1, emptyBoard(pos1 + 1).updated(pos2, Stone.Black).updated(pos2+1, Stone.White)) // Platziere den zweiten schwarzen Stein
+        updatedBoard
+
+    })
+    
     
   }
     override def toString: String = {
