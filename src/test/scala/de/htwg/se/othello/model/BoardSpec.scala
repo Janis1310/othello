aceee7150e392e5a03ca3afe74a22c8885d59825
imranjan75 <janimran7599@gmail.com>
2024-10-24 10:46:09 +0200

    create test class for board

---
+++ src/test/scala/de/htwg/se/othello/model/BoardSpec.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.othello.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class BoardSpec extends AnyWordSpec with Matchers {
+    
+}
