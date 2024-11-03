99c1dbd69fd454e400aa7d59b6aa2959f140ba5e
imranjan75 <janimran7599@gmail.com>
2024-11-03 15:34:57 +0100

    control postion of stone

--- src/main/scala/de/htwg/se/othello/model/moveValidator.scala
+++ src/main/scala/de/htwg/se/othello/model/moveValidator.scala
@@ -1,18 +1,50 @@
 package de.htwg.se.othello.model
 
 object moveValidator {
-    def isValidMove(x: Int, y: Int, color: Stone, board: Board): Boolean = {
-        if ( board.getBoard.cell(x, y)!= Stone.Empty) { //field is empty?
-            println("field is not empty")
+    def isValidMove(stoneposition: Stoneposition, board: Board): Boolean = {
+        if ( board.getBoard.cell(stoneposition.x, stoneposition.y)!= Stone.Empty) { //field is empty?
+            println("Field is not empty")
             return false
         }
 
-        // if (board(x+1,y) != color.flip) { // das müssen wir noch rinksrum prüfen
-        //     println(board.getStoneAt(x+1,y))
-        //     println("field is not flippable")
-        //     return false
-        // }
-        // Prüfung fehlt noch
-        return true
+        if(stoneposition.x < 0 || stoneposition.x >= board.getBoard.numRows || stoneposition.y < 0 || stoneposition.y >= board.getBoard.numCols) {
+          printf("Ungültige Position: (" + "%d, %d)", stoneposition.x, stoneposition.y)
+          return false
+        }
+
+        val opponent = if (stoneposition.stone == Stone.White) Stone.Black else Stone.White
+        val directions = Seq(
+          (0, 1),   // rechts
+          (1, 1),   // rechts unten
+          (1, 0),   // unten
+          (1, -1),  // links unten
+          (0, -1),  // links
+          (-1, -1), // links oben
+          (-1, 0),  // oben
+          (-1, 1)   // rechts oben
+        )
+        directions.exists { case (dx, dy) =>
+      checkDirection(dx, dy, stoneposition.x + dx, stoneposition.y + dy, false, opponent)
     }
-}
+        
+
+      return false
+    }
+
+     def checkDirection(dx: Int, dy: Int, x: Int, y: Int, foundOpponent: Boolean, opponent: Stone): Boolean = {
+    if (x < 0 || x >= board.numRows || y < 0 || y >= board.numCols) {
+      return false // Aus dem Board heraus
+    }
+
+    board.cell(x, y) match {
+      case `opponent` => checkDirection(dx, dy, x + dx, y + dy, true, opponent) // Gegnerstein gefunden
+      case `player` if foundOpponent => true // Gültiger Zug gefunden
+      case Stone.Empty => false // Leeres Feld, keine gültige Reihe
+      case _ => false // Eigenes Feld ohne Gegnersteine
+    }
+  }
+
+  }
+
+        
+   
