76de2e0c6e3b1c210f153c1f9d5eafbbb887c432
imranjan75 <janimran7599@gmail.com>
2024-11-03 12:13:43 +0100

    adjust board class with matrix

--- src/main/scala/de/htwg/se/othello/model/Board.scala
+++ src/main/scala/de/htwg/se/othello/model/Board.scala
@@ -1,44 +1,47 @@
 package de.htwg.se.othello.model
 
-class Board(private val board: Vector[Vector[Stone]]) {
+class Board(private val board: Matrix[Stone]) {
 
     def this(row: Int, column: Int) = {
     this({
-        val emptyBoard = Vector.fill(row, column)(Stone.Empty)
+       val emptyBoard = new Matrix(row, column, Stone.Empty)
+
+      // Calculate center positions for the initial stones
         val pos1 = (row - 1) / 2
-        val pos2 = ( column - 1) / 2
+        val pos2 = (column - 1) / 2
 
-        val updatedBoard = emptyBoard
-        .updated(pos1, emptyBoard(pos1).updated(pos2, Stone.White).updated(pos2 + 1,Stone.Black)) // Platziere den ersten weißen Stein
-        .updated(pos1 + 1, emptyBoard(pos1 + 1).updated(pos2, Stone.Black).updated(pos2+1, Stone.White)) // Platziere den zweiten schwarzen Stein
-        updatedBoard
-
+      // Place the initial stones on the board
+        emptyBoard
+          .replaceCell(pos1, pos2, Stone.White)         // Top-left of the center
+          .replaceCell(pos1, pos2 + 1, Stone.Black)     // Top-right of the center
+          .replaceCell(pos1 + 1, pos2, Stone.Black)     // Bottom-left of the center
+          .replaceCell(pos1 + 1, pos2 + 1, Stone.White) // Bottom-right of the center
     })
   }
   
     override def toString: String = {
-        val boardmaxcol = board(0).length
-        val boardmaxrow = board.indices
+       val numRows = board.numRows
+       val numCols = board.numCols
 
         val sb = new StringBuilder
         sb.append("    ") 
-        for (i <- 0 until boardmaxcol) {
+        for (i <- 0 until numCols) {
           sb.append(s"$i   ")
         }
         sb.append("\n")
 
-        for (row <- boardmaxrow) {
+        for (row <- 0 until numRows) {
           sb.append("  ")
-          sb.append("+---" * boardmaxcol)
-          sb.append("+\r\n")
-          sb.append(s"$row | ") 
-          for (col <- board(row).indices) {
-            sb.append(s"${board(row)(col).toString} | ")
+          sb.append("+---" * numCols)
+          sb.append("+\n")
+          sb.append(s"$row | ")
+          for (col <- 0 until numCols) {
+            sb.append(s"${board.cell(row, col).toString} | ")
           }
           sb.append("\n")
         }
         sb.append("  ")
-        sb.append("+---" * boardmaxcol) 
+        sb.append("+---" * numCols)
         sb.append("+\n")
 
         sb.toString()
