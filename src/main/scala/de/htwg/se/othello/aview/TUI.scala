745e49afac4aaeedc7a8c59967c49dfc9dec2070
imranjan75 <janimran7599@gmail.com>
2024-11-06 22:11:59 +0100

    TUI class

--- src/main/scala/de/htwg/se/othello/aview/TUI.scala
+++ src/main/scala/de/htwg/se/othello/aview/TUI.scala
@@ -1,5 +1,40 @@
 package de.htwg.se.othello.aview
 
+import de.htwg.se.othello.model.{Player, Stone, Board, MoveHandler, Stoneposition}
+import scala.io.StdIn.readLine
+import scala.collection.immutable.Queue
+import de.htwg.se.othello.model.Player
+
 class TUI {
+
+  def inputPlayerName(): Queue[Player] = {
+    val player1 = Player(readPlayerName(1), Stone.White)
+    val player2 = Player(readPlayerName(2), Stone.Black)
+    Queue(player1, player2) // Rückgabe des erstellten Queue
+  }
+
+  def inputBoardSize(): (Int, Int) = {
+    val Array(row, column) = readLine("Geben Sie die Größe des Feldes x,y: ").split(",").map(_.trim.toInt)
+    (row, column) 
+  }
+
+    private def readPlayerName(playerNumber: Int): String = {
+    println(s"Geben Sie den Namen des Spielers $playerNumber ein:")
+    readLine()
+  }
+
+  def playTurn(board: Board, currentPlayer: Player) : Board = {
+
+    val Array(x, y) = readLine("Gib die Koordinaten für deinen Zug im Format x,y ein: ").split(",").map(_.trim.toInt)
+    
+    val newBoard = if (MoveHandler.isValidMove(Stoneposition(x, y, currentPlayer.stone), board)) {
+      MoveHandler.flipStones(Stoneposition(x, y, currentPlayer.stone), board)
+    } else {
+      println("Ungültiger Zug. Bitte versuche es erneut.")
+      board
+    }
+
+    newBoard
+  }
   
 }
