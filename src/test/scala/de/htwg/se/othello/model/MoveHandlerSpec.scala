2aad94e5a269c62179e8df2e3b64d3d77b82f228
Janis1310 <janis1310@gmail.com>
2024-11-07 13:38:37 +0100

    new MoveHandlerSpec.scala

---
+++ src/test/scala/de/htwg/se/othello/model/MoveHandlerSpec.scala
@@ -0,0 +1,25 @@
+
+package de.htwg.se.othello.model
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.othello.model.{Matrix, Stone}
+
+class MoveHandlerSpec extends AnyWordSpec {
+    "A Move " when {
+        "isValidMove" should {
+            "be false when field is full" in {
+                val board = new Board(8,8)
+                board.isValidMove(3,3,Stone.Black) should be(false)
+            }
+            "be false when field is out of bounds" in {
+                val board = new Board(8,8)
+                board.isValidMove(9,9,Stone.Black) should be(false)
+            }
+            
+
+        }
+    }
+}
+
+
