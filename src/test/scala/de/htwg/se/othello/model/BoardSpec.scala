65595e73e7ed5239090530d8744ca7fea380035c
imranjan75 <janimran7599@gmail.com>
2024-11-07 19:30:07 +0100

    Tests for added functions

--- src/test/scala/de/htwg/se/othello/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/othello/model/BoardSpec.scala
@@ -30,9 +30,33 @@ class BoardSpec extends AnyWordSpec with Matchers {
         7 | . | . | . | . | . | . | . | . |
           +---+---+---+---+---+---+---+---+ """
 
-        println(board.toString()) // Gibt die tatsächliche Ausgabe des Boards aus
-        board.toString().replaceAll("\\s+", "") shouldBe initialBoardStr.replaceAll("\\s+", "")
+      println(board.toString()) // Gibt die tatsächliche Ausgabe des Boards aus
+      board.toString().replaceAll("\\s+", "") shouldBe initialBoardStr.replaceAll("\\s+", "")
     }
-    
+
+    "place a stone correctly on the board" in {
+      // Erstelle ein neues Board-Objekt mit einer Standardgröße (z.B. 8x8)
+      val board = new Board(8, 8)
+
+      // Platziere einen Stein (z.B., Stone.Black) an Position (3, 3)
+      val updatedBoard = board.placeStone(3, 2, Stone.Black)
+
+      // Überprüfe, dass der Stein an der richtigen Position platziert wurde
+      updatedBoard.getBoard.cell(3, 2) shouldEqual Stone.Black
+
+      // Überprüfe, dass das ursprüngliche Board unverändert ist
+      board.getBoard.cell(3, 2) shouldEqual Stone.Empty
+    }
+
+    "return a new Board instance after placing a stone" in {
+      // Erstelle ein neues Board-Objekt
+      val board = new Board(8, 8)
+
+      // Platziere einen Stein und erhalte das aktualisierte Board
+      val updatedBoard = board.placeStone(2, 2, Stone.White)
+
+      // Überprüfe, dass ein neues Board-Objekt zurückgegeben wird
+      updatedBoard should not be theSameInstanceAs(board)
+    }
   }
 }
