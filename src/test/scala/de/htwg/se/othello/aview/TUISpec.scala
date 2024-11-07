62f5f19c21ef6420c5161c492952380463933c77
imranjan75 <janimran7599@gmail.com>
2024-11-07 22:09:44 +0100

    add import classes

--- src/test/scala/de/htwg/se/othello/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/othello/aview/TUISpec.scala
@@ -1,16 +1,13 @@
 package de.htwg.se.othello.aview
 
+import scala.io.StdIn.readLine
+import java.io.{ByteArrayInputStream, InputStream, PrintStream}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
-import de.htwg.se.othello.model.{Board, Stone} // import board and stone
+import scala.collection.immutable.Queue
+import de.htwg.se.othello.model.{Board, Stone, Player} // import board and stone
+import de.htwg.se.othello.aview.TUI
 
 class TUISpec extends AnyWordSpec with Matchers {
-  "A TUI" should {
-
-    "Input player name correctly" in {
-      
-
-    }
-    
-  }
+  
 }
