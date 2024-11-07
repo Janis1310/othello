97ccbdce7ea7bf3d934be3d68a3a930c7e4e73c6
imranjan75 <janimran7599@gmail.com>
2024-11-07 12:45:34 +0100

    main

--- src/main/scala/de/htwg/se/othello/model/Othello.scala
+++ src/main/scala/de/htwg/se/othello/model/Othello.scala
@@ -5,29 +5,29 @@ import de.htwg.se.othello.aview.TUI
 object Othello {
   def main(args: Array[String]): Unit = {
 
-    // val tui = new TUI
-    // var players = tui.inputPlayerName()
-    // val (row, column) = tui.inputBoardSize()
-    // var board = new Board(row, column)
+    val tui = new TUI
+    var players = tui.inputPlayerName()
+    val (row, column) = tui.inputBoardSize()
+    var board = new Board(row, column)
 
-    // println("Das Spiel beginnt! Drücken Sie Strg + C, um das Spiel zu beenden.")
+    println("Das Spiel beginnt! Drücken Sie Strg + C, um das Spiel zu beenden.")
 
-    // while (true){
-    //   val currentPlayer = players.head
-    //   println(board)
+    while (true){
+      val currentPlayer = players.head
+      println(board)
 
-    //   println(s"${currentPlayer.name}, du bist am Zug! Deine Farbe ist: ${currentPlayer.stone}")
+      println(s"${currentPlayer.name}, du bist am Zug! Deine Farbe ist: ${currentPlayer.stone}")
 
-    //   board = tui.playTurn(board, currentPlayer = currentPlayer )
-    //   players = players.tail.enqueue(currentPlayer)
+      board = tui.playTurn(board, currentPlayer = currentPlayer )
+      players = players.tail.enqueue(currentPlayer)
 
-    // }
-    var board = new Board(6,6)
-    println(board)
-    board = MoveHandler.flipStones(Stoneposition(1,3,Stone.White), board = board )
-    println(board)
-     board = MoveHandler.flipStones(Stoneposition(3,4,Stone.Black), board = board )
-    println(board)
+    }
+    // var board = new Board(6,6)
+    // println(board)
+    // board = MoveHandler.flipStones(Stoneposition(1,3,Stone.White), board = board )
+    // println(board)
+    //  board = MoveHandler.flipStones(Stoneposition(3,4,Stone.Black), board = board )
+    // println(board)
 
 
   }
