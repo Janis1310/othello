73e53807b6c08fe9272d412aecf1f305fc5adf46
imranjan75 <janimran7599@gmail.com>
2024-11-15 16:21:15 +0100

    adjust main class

--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -7,25 +7,14 @@ import de.htwg.se.othello.controller.Controller
 object Othello {
   val controller = new Controller(new Board(8, 8))
   val tui = new TUI(controller)
-  controller.notifyObservers
 
   def main(args: Array[String]): Unit = {
-    var players = tui.inputPlayerName()
-    val (row, column) = tui.inputBoardSize()
-    var board = new Board(row, column)
-
-    controller.board = board
+    tui.inputPlayers()
+    tui.inputBoardSize()
 
     println("Das Spiel beginnt! Drücken Sie Strg + C, um das Spiel zu beenden.")
-    while (true){
-      val currentPlayer = players.head
-      println(board)
-
-      println(s"${currentPlayer.name}, du bist am Zug! Deine Farbe ist: ${currentPlayer.stone}")
-
-      board = tui.playTurn(board, currentPlayer = currentPlayer )
-      players = players.tail.enqueue(currentPlayer)
-
+    while (true) {
+      tui.playTurn()
     }
   }
 }
