0a2c0229fa38ccb43b1844b76f94c55177105d0e
imranjan75 <janimran7599@gmail.com>
2024-11-18 14:54:59 +0100

    adjust main class in branch development

--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -1,70 +1,22 @@
 package de.htwg.se.othello
 
-import scala.io.StdIn._
+import de.htwg.se.othello.aview.TUI
+import de.htwg.se.othello.model.Board
+import de.htwg.se.othello.controller.Controller
+
 object Othello {
-  def main(args:Array[String]) = {
-    println("Welcome to Othello ")
+  val controller = new Controller(new Board(8, 8))
+  val tui = new TUI(controller)
 
-    val p1_name = readLine("Geben Sie den Name vom Player1 ein: ")
-    val p2_name = readLine("Geben Sie den Name vom Player2 ein: ")
-    val p1 = Player(p1_name, "W")
-    val p2 = Player(p2_name, "S")
+  def main(args: Array[String]): Unit = {
+    tui.inputPlayers()
+    tui.inputBoardSize()
 
-    val feld = readLine("Geben Sie die Feldgröße getrennt durch ein Komma ein: ")
-    val Array(row, column) = feld.split(",").map(_.trim.toInt)
-
-    val board = createboard(row, column)
-    printboard(board)
-    //test
-
-  }
-
-  def createboard(row:Int, column:Int): Array[Array[String]] = {
-    val board = Array.fill(row, column)(".")
-    val pos1 = (row - 1) / 2
-    val pos2 = ( column - 1) / 2
-
-    board(pos1)(pos2) = "W"
-    board(pos1)(pos2+1) = "B"
-    board(pos1+1)(pos2) = "B" 
-    board(pos1+1)(pos2+1) = "W"
-    board
-  
-  }
-
-  // print the board
-  def printboard(board: Array[Array[String]]) : Unit = {
-    val boardmaxcol = board(0).length
-    val boardmaxrow = board.indices
-
-    print("    ")
-    board(0).indices.foreach(i => print(s"$i   "))
-    println()
-    
-    for (row <- boardmaxrow) {
-      print("  ")
-
-      print("+---" * boardmaxcol)
-      println("+")
-      print(s"$row | ") 
-      for (col <- board(row).indices) {
-        print(s"${board(row)(col)} | ") 
-      }
-      println()
+    println("Das Spiel beginnt! Drücken Sie Strg + C, um das Spiel zu beenden.")
+    while (true) {
+      tui.playTurn()
     }
-    print("  ")
-    print("+---" * boardmaxcol)
-    println("+")
-
   }
-
-
-}
-
-// create player class
-case class Player(name:String, color:String){
-    override def toString(): String = name +": " + color
-
 }
 
 
