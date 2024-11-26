6a1404914050948acfcd2be04d0545c94071c61e
Janis1310 <janis1310@gmail.com>
2024-11-26 12:50:45 +0100

    start strategy.scala

--- src/main/scala/de/htwg/se/othello/ai/Strategy.scala
+++ src/main/scala/de/htwg/se/othello/ai/Strategy.scala
@@ -1,3 +1,10 @@
-package de.htwg.ai
+package de.htwg.se.othello.ai
 
+object StrategyContext {
+    var strategy = if (Random.nextInt() % 2 == 0) strategy1 else strategy2
 
+    def strategy1 = 
+    def strategy2 =
+}
+
+StrategyContext.strategy
