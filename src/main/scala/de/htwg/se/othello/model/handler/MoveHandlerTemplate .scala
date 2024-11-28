bbefc8d34a85daf6b51fbc04d15b80f2ae616abd
imranjan75 <janimran7599@gmail.com>
2024-11-28 12:38:33 +0100

    implemente move handler template

--- src/main/scala/de/htwg/se/othello/model/handler/MoveHandlerTemplate .scala
+++ src/main/scala/de/htwg/se/othello/model/handler/MoveHandlerTemplate .scala
@@ -2,7 +2,18 @@ package de.htwg.se.othello.model.handler
 
 import de.htwg.se.othello.model.{Stoneposition, Board}
 
-trait MoveHandlerInterface {
-  def isValidMove(stonePosition: Stoneposition, board: Board): Boolean
-  def flipStones(stonePosition: Stoneposition, board: Board): Board
+abstract class MoveHandlerTemplate {
+  // Template-Methode: Die Hauptlogik, die von allen konkreten Klassen verwendet wird.
+  final def processMove(stonePosition: Stoneposition, board: Board): Board = {
+    if (!isValidMove(stonePosition, board)) {
+      throw new IllegalArgumentException()
+    }
+
+    val updatedBoard = flipStones(stonePosition, board)
+    updatedBoard
+  }
+
+  // Schritte, die von den Unterklassen implementiert werden müssen.
+  protected def isValidMove(stonePosition: Stoneposition, board: Board): Boolean
+  protected def flipStones(stonePosition: Stoneposition, board: Board): Board
 }
