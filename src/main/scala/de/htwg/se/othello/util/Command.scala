bac5d7c745a8e266d3ff8167717e9eb79d4c666c
Janis1310 <janis1310@gmail.com>
2024-12-04 14:16:40 +0100

    new SetCommand.scala

--- src/main/scala/de/htwg/se/othello/util/Command.scala
+++ src/main/scala/de/htwg/se/othello/util/Command.scala
@@ -5,9 +5,3 @@ trait Command {
     def undoStep:Unit
     def redoStep:Unit
 }
-
-class SetCommand(row:Int, col: Int, value:Int, controller: Controller) extends Command {
- override def doStep: Unit =   controller.grid = controller.grid.set(row, col, value)
- override def undoStep: Unit = controller.grid = controller.grid.set(row, col, 0)
- override def redoStep: Unit = controller.grid = controller.grid.set(row, col, value)
-}
