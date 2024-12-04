d1267723c3c3aae4662dd7d2e0d64bd8e66ce525
Janis1310 <janis1310@gmail.com>
2024-12-04 14:17:07 +0100

    new SetCommand.scala

--- src/main/scala/de/htwg/se/othello/controller/SetCommand.scala
+++ src/main/scala/de/htwg/se/othello/controller/SetCommand.scala
@@ -1,3 +1,12 @@
 package de.htwg.se.othello.controller
 
 
+import de.htwg.se.sudoku.util.Command
+
+class SetCommand(row:Int, col: Int, value:Int, controller: Controller) extends Command {
+  override def doStep: Unit =   controller.grid = controller.grid.set(row, col, value)
+
+  override def undoStep: Unit = controller.grid = controller.grid.set(row, col, 0)
+
+  override def redoStep: Unit = controller.grid = controller.grid.set(row, col, value)
+}
