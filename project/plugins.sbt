f7a02e0831090374df65a61e92e513cdb13889fd
Janis1310 <janis1310@gmail.com>
2024-12-08 19:22:21 +0100

    adding test GUI

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.2.1")
+//addSbtPlugin("org.openjfx" % "javafx-plugin" % "0.0.13")
--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.othello
 
-import de.htwg.se.othello.aview.TUI
+import de.htwg.se.othello.aview.{TUI, Gui}
 import de.htwg.se.othello.model.Board
 import de.htwg.se.othello.controller.Controller
 import scala.util.{Try, Success, Failure}
@@ -10,9 +10,11 @@ import scala.io.StdIn.readLine
 object Othello {
   val controller = new Controller(new Board(8, 8))
   val tui = new TUI(controller)
+  //val gui = new Gui(controller)
 
   def main(args: Array[String]): Unit = {
 
     tui.start()
+    Gui.main(args)
   }
 }
