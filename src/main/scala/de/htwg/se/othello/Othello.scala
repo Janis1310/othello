a991d20e5357b132ca8aec8baf2a530149b16f68
imranjan75 <janimran7599@gmail.com>
2024-12-11 19:39:21 +0100

    adjust main for swing

--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -6,16 +6,21 @@ import de.htwg.se.othello.controller.Controller
 import scala.util.{Try, Success, Failure}
 
 import scala.io.StdIn.readLine
+import de.htwg.se.othello.aview.GUI.OthelloGUI
 
 object Othello {
   val controller = new Controller(new Board(8, 8))
   val tui = new TUI(controller)
+  val gui = new OthelloGUI(controller)
+
   //val gui = new Gui(controller)
 
   def main(args: Array[String]): Unit = {
-    GuiMain.setController(controller)
-    GuiMain.main(args)
-    tui.start()
-    GuiMain.main(args)
+    // GuiMain.setController(controller)
+    // GuiMain.main(args)
+    // tui.start()
+    // GuiMain.main(args)
+    gui.start()
+
   }
 }
