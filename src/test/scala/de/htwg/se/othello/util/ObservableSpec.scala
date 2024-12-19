86b993a9a962362811986a8101912c85929b4c9a
imranjan75 <janimran7599@gmail.com>
2024-12-19 11:35:18 +0100

    finish tests for Observable

--- src/test/scala/de/htwg/se/othello/util/ObservableSpec.scala
+++ src/test/scala/de/htwg/se/othello/util/ObservableSpec.scala
@@ -3,13 +3,16 @@ import java.security.KeyStore.TrustedCertificateEntry
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
 class ObservableSpec extends AnyWordSpec {
+  class TestObserver extends Observer {
+    var updated: Boolean = false
+  
+    override def update: Unit = { updated = true }
+  }
+
+
   "An Observable" should {
     val observable = new Observable
-    val observer = new Observer {
-      var updated: Boolean = false
-      def isUpdated: Boolean = updated
-      override def update: Unit = {updated = true}
-    }
+    val observer = new TestObserver
     "add an Observer" in {
       observable.add(observer)
       observable.subscribers should contain (observer)
@@ -20,6 +23,23 @@ class ObservableSpec extends AnyWordSpec {
       observable.remove(observer)
       observable.subscribers should not contain (observer)
     }
+
+    "notify all its Observers" in {
+      val observer1 = new TestObserver
+      val observer2 = new TestObserver
+
+      // Add observers to the observable
+      observable.add(observer1)
+      observable.add(observer2)
+
+      // Notify all observers
+      observable.notifyObservers
+
+      // Assert that all observers were notified
+      observer1.updated should be(true)
+      observer2.updated should be(true)
+    }
+
 
   }
 
