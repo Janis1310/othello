32c7b14f6b8dabcf6099faeb8ea47f5a6d919095
imranjan75 <janimran7599@gmail.com>
2025-01-14 14:55:55 +0100

    add writes function

--- src/main/scala/de/htwg/se/othello/model/Playercomponents/Player.scala
+++ src/main/scala/de/htwg/se/othello/model/Playercomponents/Player.scala
@@ -1,6 +1,8 @@
 package de.htwg.se.othello.model.Playercomponents
 
 import de.htwg.se.othello.model.BoardComponents.StoneComponent
+import play.api.libs.json._
+import de.htwg.se.othello.model.BoardComponents.BoardBaseImpl.Stone
 
 // Product (interface)
 trait Player {
@@ -18,14 +20,14 @@ trait Player {
 }
 
 // concreteProduct: HumanPlayer
-class HumanPlayer(val name: String, val stone: StoneComponent)
+case class HumanPlayer(val name: String, val stone: StoneComponent)
     extends Player {
   val role: String = "Human" // Define the role for HumanPlayer
   override def toString: String = s"$name: $stone"
 }
 
 // concreteProduct: AIPlayer
-class AIPlayer(val name: String, val stone: StoneComponent)
+case class AIPlayer(val name: String, val stone: StoneComponent)
     extends Player {
   val role: String = "AI" // Define the role for AIPlayer
   override def toString: String = s"$name (AI): $stone"
@@ -36,4 +38,21 @@ object Player {
     case "Human" => new HumanPlayer(name, stone)
     case "AI"    => new AIPlayer(name, stone)
   }
+
+  implicit val playerWrites: Writes[Player] = new Writes[Player] {
+  override def writes(player: Player): JsValue = player match {
+    case human: HumanPlayer =>
+      Json.obj(
+        "name" -> human.name,
+        "stone" -> Json.toJson(human.stone),
+        "role" -> human.role
+      )
+    case ai: AIPlayer =>
+      Json.obj(
+        "name" -> ai.name,
+        "stone" -> Json.toJson(ai.stone),
+        "role" -> ai.role
+      )
+  }
+}
 }
